eba308cfc622ea1f3fef24d31554c443a6063ce5
GudkovMV <92GudkovMV@region.cbr.ru>
2023-04-18 11:36:59 +0300

    Решение Саида

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import net.jpountz.lz4.LZ4Factory;
+import net.jpountz.lz4.LZ4FrameOutputStream;
+import net.jpountz.xxhash.XXHashFactory;
 import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
 import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
 import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
@@ -13,8 +16,11 @@ import org.apache.commons.compress.utils.IOUtils;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
 import java.util.InputMismatchException;
 import java.util.List;
+import java.util.Locale;
 
 /**
  * Hello world!
@@ -29,8 +35,7 @@ public class App
         if (args.length == 0){
             throw new IllegalArgumentException();
         }
-        File decompressionDir = new File("./decompressed");
-        decompressionDir.mkdir();
+        long startTime = System.nanoTime();
 
         for (String arg: args){
             File file = new File(arg);
@@ -42,34 +47,22 @@ public class App
                 }
             }
         }
-        decompressionDir.delete();
-
+        long endTime = System.nanoTime();
+        long durationInSeconds = (endTime - startTime) / 1000000000;
+        System.out.println(durationInSeconds);
     }
 
     public static void extractTarGZ(InputStream in, File root) {
-        String destName = root.getName().replaceAll("\\.tar","").replaceAll("\\.gz","");
+        String destName = root.getName().replaceAll("\\.gz","");
         try {
             GzipCompressorInputStream gzipIn = new GzipCompressorInputStream(in);
+            LZ4Factory lz4Factory = LZ4Factory.fastestJavaInstance();
+            LZ4FrameOutputStream.FLG.Bits[] bits = new LZ4FrameOutputStream.FLG.Bits[]{LZ4FrameOutputStream.FLG.Bits.BLOCK_INDEPENDENCE};
+            LZ4FrameOutputStream frameOutputStream = new LZ4FrameOutputStream(new FileOutputStream(destName+".lz4", true), LZ4FrameOutputStream.BLOCKSIZE.SIZE_4MB,
+                    -1L, lz4Factory.fastCompressor(), XXHashFactory.safeInstance().hash32(), bits);
 
-            try (TarArchiveInputStream tarIn = new TarArchiveInputStream(gzipIn)) {
-                while (( tarIn.getNextEntry()) != null) {
-                    /** If the entry is a directory, create the directory. **/
-                        int count;
-                    BlockLZ4CompressorOutputStream fos = new BlockLZ4CompressorOutputStream(new FileOutputStream(destName+".lz4", true));
-
-                    byte data[] = new byte[BUFFER_SIZE];
-                        try (BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER_SIZE)) {
-                            while ((count = tarIn.read(data, 0, BUFFER_SIZE)) != -1) {
-                                dest.write(data, 0, count);
-                            }
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                }
-
-                System.out.println("Untar completed successfully!");
-            }
+            IOUtils.copy(gzipIn, frameOutputStream);
+            System.out.println("Untar completed successfully!");
         } catch (Exception e) {
             System.out.println(e);
         }
